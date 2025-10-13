6fb4772781a4235630b347c16931065589e7550f
Volodumur <volkapro20@gmail.com>
2025-10-13 13:44:55 +0300

    fix few bugs with humburger

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
-'use sctict'
+"use sctict";
+document.addEventListener("DOMContentLoaded", () => {
+	const buttons = document.querySelectorAll(".scroll");
 
-const buttons = document.querySelectorAll('.scroll');
-
-buttons.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    const target = document.getElementById('price');
-    if (target) {
-      target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    }
-  });
-});
-
-AOS.init({
-  duration: 2000,   
-    once: true,       
-    offset: 100  
-});
-
-const burger = document.querySelector('.hamburger');
-	const menu = document.querySelector('.menu_nav');
-  const social = document.querySelector('.header_social');
-
-	burger.addEventListener('click', () => {
-		burger.classList.toggle('active');
-		menu.classList.toggle('active');
-    social.classList.toggle('active');
-		document.body.classList.toggle('lock'); 
+	buttons.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			const target = document.getElementById("price");
+			if (target) {
+				target.scrollIntoView({
+					behavior: "smooth",
+					block: "start",
+				});
+			}
+		});
 	});
+
+	if (window.innerWidth > 1024) {
+		AOS.init({
+			duration: 2000,
+			once: true,
+			offset: 100,
+		});
+	}
+
+	const burger = document.querySelector(".hamburger");
+	const menu = document.querySelector(".menu_nav");
+	const social = document.querySelector(".header_social");
+
+	burger.addEventListener("click", () => {
+		burger.classList.toggle("active");
+		menu.classList.toggle("active");
+		social.classList.toggle("active");
+		document.body.classList.toggle("lock");
+	});
+});
